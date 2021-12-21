0965137cbe1154442672f2e4d2a6e5c9c1b418a9
TianYuan <tianyuan.hu@tuya.com>
2021-12-21 14:45:00 +0800

    test lint

--- cp-tmpl.js
+++ cp-tmpl.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const path = require('path')
 
 exec('rm -rf ./dist/tmp && cp -R tmp ./dist/tmp', {
-	cwd: path.resolve('./')
+	   cwd: path.resolve('./')
 }, (err) => {
 	if (err) {
 		console.log(err)
